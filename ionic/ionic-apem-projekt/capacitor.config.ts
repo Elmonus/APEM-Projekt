6674b3272867ecb8c3e352ff34b417de620890c3
RKutnik <62057785+RKutnik@users.noreply.github.com>
2025-05-30 19:26:42 +0200

    usunięcie starej wersji w ionic/ionic-apem-projekt

--- ionic/ionic-apem-projekt/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-apem-projekt',
-  webDir: 'dist'
-};
-
-export default config;
